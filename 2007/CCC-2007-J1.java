4c6fed4a40e036e4987e6dab39a2cb3cf6a55617
Martin Tin <75148357+tin-martin@users.noreply.github.com>
2021-10-24 01:33:11 -0400

    Update CCC-2007-J1.java

--- 2007/CCC-2007-J1.java
+++ 2007/CCC-2007-J1.java
@@ -1,13 +1,16 @@
+/**
+Use Collections.sort() to sort weights in ascending order. Print out middle number of sorted array.
+**/
 import java.util.*;
 public class test{
  
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int[] w = {sc.nextInt(), sc.nextInt(),sc.nextInt()};
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        a.add(w[0]);
-        a.add(w[1]);
-        a.add(w[2]);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(w[0]);
+        arr.add(w[1]);
+        arr.add(w[2]);
         Collections.sort(a);
         System.out.println(a.get(1));
     }
